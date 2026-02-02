0bc702e1b54c9842d50188bd4079c0c45341c2b3
Error <nc9176538@gmail.com>
2026-02-02 20:16:30 +0500

    Create installation script for dependencies
    This script updates the system, installs Python3 and figlet, and installs required Python packages.

---
+++ install.sh
@@ -0,0 +1,5 @@
+
+clear                                                      echo "INSTALLATION STARTED"
+                                                           apt update && apt upgrade -y                               apt install python3 -y
+apt install figlet -y                                                                                                 pip install -r requirements.txt
+clear                                                                                                                 echo "INSTALLATION SUCCESSFUL. NOW RUN 'python details.py'"
