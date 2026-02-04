3522f3600d4ba6170bae6afd3722eed5080878be
Error <nc9176538@gmail.com>
2026-02-04 15:04:21 +0500

    Refactor installation script for clarity and updates

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 
-clear                                                      echo "INSTALLATION STARTED"
-                                                           apt update && apt upgrade -y                               apt install python3 -y
-apt install figlet -y                                                                                                 pip install -r requirements.txt
-clear                                                                                                                 echo "INSTALLATION SUCCESSFUL. NOW RUN 'python details.py'"
+clear          
+echo "INSTALLATION STARTED"
+git pull
+apt update && apt upgrade -y          
+apt install python3 -y
+apt install figlet -y                                                     
+pip install -r requirements.txt
+clear                                                                                                                 
+echo "INSTALLATION SUCCESSFUL. NOW RUN 'python details.py'"
